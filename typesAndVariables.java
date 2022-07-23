95e63f0887a970a2133907c6f45241170b8dbd4b
Carissa Hibbert <108243658+carhibbert@users.noreply.github.com>
2022-07-23 14:36:52 -0500

    Add files via upload

---
+++ typesAndVariables.java
@@ -0,0 +1,29 @@
+public class typesAndVariables {
+
+// Homework Week 4 Day 2
+    public static void main(String[] args) {
+
+    //Standard Java naming convention: camelCase
+        boolean hasEaten = true;
+        boolean $food = false; // not encouraged
+
+    //Define some illegal variable names
+        // int %43 = ;
+        // boolean !!! = ;
+        // short Test(Grade) = ;
+        // string 90$ = "";
+
+    //Print to console 
+
+        System.out.println(hasEaten);
+        System.out.println($food);
+
+        // System.out.println(65);
+        // System.out.println(OKAY);
+        // System.out.println(!!!);
+        // System.out.println(Test(Grade));
+        // System.out.println(90$);
+
+    }
+
+}
